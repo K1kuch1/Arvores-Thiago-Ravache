142c8f4649cde3fa123c064d6ffd370693deb667
kikuchi <overpowerop7@gmail.com>
2025-05-15 20:16:50 -0300

    primeiro commit

---
+++ src/ArvoreBinaria.java
@@ -0,0 +1,9 @@
+public class ArvoreBinaria {
+    public No raiz;
+    public No filho;
+
+    public ArvoreBinaria(No raiz, No filho) {
+        this.raiz = raiz;
+        this.filho = filho;
+    }
+}
